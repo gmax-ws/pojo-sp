d1d68a8334a90088f10faeb0b12d3ae7828c1dda
mag <mag@mag-Inspiron-N5110>
2014-11-01 17:31:23 +0200

    make source java 1.7 compatible

--- src/main/java/gmax/spm/api/ProcedureManagerImpl.java
+++ src/main/java/gmax/spm/api/ProcedureManagerImpl.java
@@ -31,308 +31,336 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * ProcedureManager and TransactionManager API.
- *
+ * 
  * @author Marius Gligor
  * @version 3.0
  */
 class ProcedureManagerImpl implements ProcedureManager, TransactionManager {
 
-    /** The JDBC Connection object */
-    private Connection connection;
+	/** Fields cache */
+	private Map<Class<?>, List<Field>> cache = new HashMap<>();
 
-    /**
-     * Default constructor.
-     */
-    public ProcedureManagerImpl() {
-    }
+	/** The JDBC Connection object */
+	private Connection connection;
 
-    /**
-     * Construct a ProcedureManager instance using a JDBC Connection.
-     *
-     * @param 	connection
-     * 			JDBC Connection object.
-     */
-    public ProcedureManagerImpl(Connection connection) {
-        this.connection = connection;
-    }
+	/**
+	 * Default constructor.
+	 */
+	public ProcedureManagerImpl() {
+	}
 
-    /**
-     * Get connection.
-     *
-     * @return 	JDBC Connection object.
-     */
-    @Override
-    public Connection getConnection() {
-        return connection;
-    }
+	/**
+	 * Construct a ProcedureManager instance using a JDBC Connection.
+	 * 
+	 * @param 	connection
+	 *       	JDBC Connection object.
+	 */
+	public ProcedureManagerImpl(Connection connection) {
+		this.connection = connection;
+	}
 
-    /**
-     * Close connection.
-     */
-    @Override
-    public void close() {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                throw new ProcedureManagerException(e);
-            }
-        }
-    }
+	/**
+	 * Get connection.
+	 * 
+	 * @return JDBC Connection object.
+	 */
+	@Override
+	public Connection getConnection() {
+		return connection;
+	}
 
-    /**
-     * Get TransactionManager API.
-     *
-     * @return 	TransactionManager interface.
-     */
-    @Override
-    public TransactionManager getTransactionManager() {
-        return this;
-    }
+	/**
+	 * Close connection.
+	 */
+	@Override
+	public void close() {
+		
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new ProcedureManagerException(e);
+			}
+		}
+	}
 
-    /**
-     * Get procedure name from @StoredProcedure annotation.
-     *
-     * @param 	type 
-     * 			pojo Class.
-     * 
-     * @return 	StoredProcedure annotation.
-     */
-    private StoredProcedure getProcedureName(Class<? extends Object> type) {
-        if (!type.isAnnotationPresent(StoredProcedure.class)) {
-            throw new ProcedureManagerException("@StoredProcedure annotation is missing.");
-        }
-        return type.getAnnotation(StoredProcedure.class);
-    }
+	/**
+	 * Get TransactionManager API.
+	 * 
+	 * @return 	TransactionManager interface.
+	 */
+	@Override
+	public TransactionManager getTransactionManager() {
+		return this;
+	}
 
-    /**
-     * Get a <code>List</code> of @StoredProcedureParameter annotated fields.
-     *
-     * @param 	type 
-     * 			pojo Class.
-     * 
-     * @return 	List of StoredProcedureParameter annotated fields
-     */
-    private List<Field> getProcedureParameters(Class<? extends Object> type) {
-        List<Field> fields = new LinkedList<Field>();
-        Field[] declaredFields = type.getDeclaredFields();
-        for (Field field : declaredFields) {
-            if (field.isAnnotationPresent(StoredProcedureParameter.class)) {
-                fields.add(field);
-            }
-        }
-        return fields;
-    }
+	/**
+	 * Get procedure name from @StoredProcedure annotation.
+	 * 
+	 * @param 	type
+	 * 			pojo Class.
+	 * 
+	 * @return 	StoredProcedure annotation.
+	 */
+	private StoredProcedure getProcedureName(Class<? extends Object> type) {
 
-    /**
-     * Build a SQL-92 call statement for a stored procedure or function.
-     *
-     * @param 	procedure       
-     * 			StoredProcedure metadata.
-     * @param 	parametersCount 
-     * 			Number of parameters.
-     * 
-     * @return 	Generated call statement as string.
-     */
-    private String callStatementString(StoredProcedure procedure, int parametersCount) {
-        StringBuilder buffer = new StringBuilder("{");
-        if (!procedure.procedure()) {
-            buffer.append("? = ");
-            parametersCount--;
-        }
-        buffer.append("call ").append(procedure.name()).append("(");
-        for (int i = 0; i < parametersCount; i++) {
-            buffer.append(i == 0 ? "?" : " ,?");
-        }
-        return buffer.append(")}").toString();
-    }
+		if (!type.isAnnotationPresent(StoredProcedure.class)) {
+			throw new ProcedureManagerException(
+					"@StoredProcedure annotation is missing.");
+		}
+		
+		return type.getAnnotation(StoredProcedure.class);
+	}
 
-    /**
-     * Register the input/output parameters before the call.
-     *
-     * @param 	statement
-     * 			CallableStatement object.
-     * @param 	pojo
-     * 		    Stored procedure entity.
-     * @param 	fields    
-     * 			List of fields.
-     * 
-     * @throws 	SQLException
-     * @throws 	IllegalAccessException
-     */
-    private void setParameters(CallableStatement statement, Object pojo, List<Field> fields) throws SQLException,
-            IllegalAccessException {
-        for (Field field : fields) {
-            StoredProcedureParameter param = field.getAnnotation(StoredProcedureParameter.class);
-            switch (param.direction()) {
-                case IN:
-                    field.setAccessible(true);
-                    statement.setObject(param.index(), field.get(pojo));
-                    break;
-                case OUT:
-                    statement.registerOutParameter(param.index(), param.type());
-                    break;
-                case INOUT:
-                    field.setAccessible(true);
-                    statement.setObject(param.index(), field.get(pojo));
-                    statement.registerOutParameter(param.index(), param.type());
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+	/**
+	 * Get a <code>List</code> of @StoredProcedureParameter annotated fields.
+	 * 
+	 * @param 	type
+	 *          pojo Class.
+	 * 
+	 * @return List of StoredProcedureParameter annotated fields
+	 */
+	private List<Field> getProcedureParameters(Class<? extends Object> type) {
+		
+		List<Field> fields = cache.get(type);
+		
+		if (fields == null) {
+			
+			fields = new LinkedList<Field>();
+			cache.put(type, fields);
+			
+			Field[] declaredFields = type.getDeclaredFields();
+			for (Field field : declaredFields) {
+				if (field.isAnnotationPresent(StoredProcedureParameter.class)) {
+					fields.add(field);
+				}
+			}
+		}
+		
+		return fields;
+	}
 
-    /**
-     * Register the output parameters after call.
-     *
-     * @param 	statement
-     * 			CallableStatement object.
-     * @param 	pojo      
-     * 			Stored procedure entity.
-     * @param 	fields
-     * 			List of fields.
-     * 
-     * @throws 	IllegalAccessException
-     * @throws 	SQLException
-     */
-    private void getParameters(CallableStatement statement, Object pojo, List<Field> fields)
-            throws IllegalAccessException, SQLException {
-        for (Field field : fields) {
-            StoredProcedureParameter param = field.getAnnotation(StoredProcedureParameter.class);
-            switch (param.direction()) {
-                case OUT:
-                case INOUT:
-                    field.setAccessible(true);
-                    field.set(pojo, statement.getObject(param.index()));
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+	/**
+	 * Build a SQL-92 call statement for a stored procedure or function.
+	 * 
+	 * @param 	procedure
+	 *        	StoredProcedure metadata.
+	 * @param 	parametersCount
+	 * 			Number of parameters.
+	 * 
+	 * @return 	Generated call statement as string.
+	 */
+	private String callStatementString(StoredProcedure procedure,
+			int parametersCount) {
+		
+		StringBuilder buffer = new StringBuilder("{");
+		
+		if (!procedure.procedure()) {
+			buffer.append("? = ");
+			parametersCount--;
+		}
+		
+		buffer.append("call ").append(procedure.name()).append("(");
+		for (int i = 0; i < parametersCount; i++) {
+			buffer.append(i == 0 ? "?" : " ,?");
+		}
+		
+		return buffer.append(")}").toString();
+	}
 
-    /**
-     * Call the function or stored procedure.
-     *
-     * @param 	pojo 
-     * 			Entity object.
-     */
-    @Override
-    public void call(Connection connection, Object pojo) {
-        this.connection = connection;
-        call(pojo);
-    }
+	/**
+	 * Register the input/output parameters before the call.
+	 * 
+	 * @param 	statement
+	 *          CallableStatement object.
+	 * @param 	pojo
+	 *          Stored procedure entity.
+	 * @param 	fields
+	 *          List of fields.
+	 * 
+	 * @throws 	SQLException
+	 * @throws 	IllegalAccessException
+	 */
+	private void setParameters(CallableStatement statement, Object pojo,
+			List<Field> fields) throws SQLException, IllegalAccessException {
+		
+		for (Field field : fields) {
+			StoredProcedureParameter param = field
+					.getAnnotation(StoredProcedureParameter.class);
+			
+			switch (param.direction()) {
+				case IN:
+					field.setAccessible(true);
+					statement.setObject(param.index(), field.get(pojo));
+					break;
+				case OUT:
+					statement.registerOutParameter(param.index(), param.type());
+					break;
+				case INOUT:
+					field.setAccessible(true);
+					statement.setObject(param.index(), field.get(pojo));
+					statement.registerOutParameter(param.index(), param.type());
+					break;
+				default:
+					break;
+			}
+		}
+	}
 
-    /**
-     * Call the function or stored procedure.
-     *
-     * @param 	pojo 
-     * 			POJO entity.
-     */
-    @Override
-    public void call(Object pojo) {
-        if (pojo != null) {
-            // get name and parameters
-            Class<? extends Object> type = pojo.getClass();
-            StoredProcedure procedure = getProcedureName(type);
-            List<Field> fields = getProcedureParameters(type);
-            // call procedure
-            CallableStatement statement = null;
-            try {
-                String sql = callStatementString(procedure, fields.size());
-                statement = connection.prepareCall(sql);
-                setParameters(statement, pojo, fields);
-                statement.execute();
-                getParameters(statement, pojo, fields);
-            } catch (SQLException e) {
-                throw new ProcedureManagerException(e);
-            } catch (IllegalAccessException e) {
-                throw new ProcedureManagerException(e);
-            } finally {
-                cleanUp(statement);
-            }
-        }
-    }
+	/**
+	 * Register the output parameters after call.
+	 * 
+	 * @param 	statement
+	 *          CallableStatement object.
+	 * @param 	pojo
+	 *          Stored procedure entity.
+	 * @param 	fields
+	 *          List of fields.
+	 * 
+	 * @throws 	IllegalAccessException
+	 * @throws 	SQLException
+	 */
+	private void getParameters(CallableStatement statement, Object pojo,
+			List<Field> fields) throws IllegalAccessException, SQLException {
+		
+		for (Field field : fields) {
+			StoredProcedureParameter param = field
+					.getAnnotation(StoredProcedureParameter.class);
+			switch (param.direction()) {
+			case OUT:
+			case INOUT:
+				field.setAccessible(true);
+				field.set(pojo, statement.getObject(param.index()));
+				break;
+			default:
+				break;
+			}
+		}
+	}
 
-    /**
-     * Close statement.
-     *
-     * @param 	statement 
-     * 			CallableStatement object.
-     */
-    private void cleanUp(CallableStatement statement) {
-        if (statement != null) {
-            try {
-                statement.close();
-            } catch (SQLException e) {
-                throw new ProcedureManagerException(e);
-            }
-        }
-    }
+	/**
+	 * Call the function or stored procedure.
+	 * 
+	 * @param 	pojo
+	 *          Entity object.
+	 */
+	@Override
+	public void call(Connection connection, Object pojo) {
+		this.connection = connection;
+		call(pojo);
+	}
 
-    /**
-     * Start a new JDBC transaction.
-     */
-    @Override
-    public void begin() {
-        processTransaction(TransactionOperation.START);
-    }
+	/**
+	 * Call the function or stored procedure.
+	 * 
+	 * @param 	pojo
+	 *          POJO entity.
+	 */
+	@Override	
+	public void call(Object pojo) {
+		if (pojo != null) {
+			// get name and parameters
+			Class<? extends Object> type = pojo.getClass();
+			StoredProcedure procedure = getProcedureName(type);
+			List<Field> fields = getProcedureParameters(type);
+		
+			// call procedure
+			CallableStatement statement = null;
+			try {
+				String sql = callStatementString(procedure, fields.size());
+				statement = connection.prepareCall(sql);
+				setParameters(statement, pojo, fields);
+				statement.execute();
+				getParameters(statement, pojo, fields);
+			} catch (SQLException | IllegalAccessException e) {
+				throw new ProcedureManagerException(e);
+			} finally {
+				cleanUp(statement);
+			}
+		}
+	}
 
-    /**
-     * Save changes.
-     */
-    @Override
-    public void commit() {
-        processTransaction(TransactionOperation.COMMIT);
-    }
+	/**
+	 * Close statement.
+	 * 
+	 * @param 	statement
+	 *          CallableStatement object.
+	 */
+	private void cleanUp(CallableStatement statement) {
 
-    /**
-     * Undo the changes.
-     */
-    @Override
-    public void rollback() {
-        processTransaction(TransactionOperation.ROLLBACK);
-    }
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				throw new ProcedureManagerException(e);
+			}
+		}
+	}
 
-    /**
-     * Stop a JDBC transaction.
-     */
-    @Override
-    public void end() {
-        processTransaction(TransactionOperation.STOP);
-    }
+	/**
+	 * Start a new JDBC transaction.
+	 */
+	@Override
+	public void begin() {
+		processTransaction(TransactionOperation.START);
+	}
 
-    /**
-     * Process transaction.
-     *
-     * @param 	operation 
-     * 			Transaction operation.
-     */
-    private void processTransaction(TransactionOperation operation) {
-        if (connection == null) {
-            throw new ProcedureManagerException("JDBC connection is missing.");
-        }
-        try {
-            switch (operation) {
-                case START:
-                    connection.setAutoCommit(false);
-                    break;
-                case COMMIT:
-                    connection.commit();
-                    break;
-                case ROLLBACK:
-                    connection.rollback();
-                    break;
-                default:
-                    connection.setAutoCommit(true);
-                    break;
-            }
-        } catch (SQLException e) {
-            throw new ProcedureManagerException(e);
-        }
-    }
+	/**
+	 * Save changes.
+	 */
+	@Override
+	public void commit() {
+		processTransaction(TransactionOperation.COMMIT);
+	}
+
+	/**
+	 * Undo the changes.
+	 */
+	@Override
+	public void rollback() {
+		processTransaction(TransactionOperation.ROLLBACK);
+	}
+
+	/**
+	 * Stop a JDBC transaction.
+	 */
+	@Override
+	public void end() {
+		processTransaction(TransactionOperation.STOP);
+	}
+
+	/**
+	 * Process transaction.
+	 * 
+	 * @param 	operation
+	 *          Transaction operation.
+	 */
+	private void processTransaction(TransactionOperation operation) {
+		if (connection == null) {
+			throw new ProcedureManagerException("JDBC connection is missing.");
+		}
+		try {
+			switch (operation) {
+			case START:
+				connection.setAutoCommit(false);
+				break;
+			case COMMIT:
+				connection.commit();
+				break;
+			case ROLLBACK:
+				connection.rollback();
+				break;
+			default:
+				connection.setAutoCommit(true);
+				break;
+			}
+		} catch (SQLException e) {
+			throw new ProcedureManagerException(e);
+		}
+	}
 }
